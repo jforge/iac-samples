e7d1b6396d7654c31946a9c45497b9a2be6533b6
jforge <github@jforge.de>
2019-11-07 23:39:50 +0100

    pulumi change test

--- pulumi/quickstart/index.ts
+++ pulumi/quickstart/index.ts
@@ -2,8 +2,20 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+// Create a KMS Key for S3 server-side encryption
+const key = new aws.kms.Key("my-key");
+
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("my-bucket", {
+  serverSideEncryptionConfiguration: {
+    rule: {
+      applyServerSideEncryptionByDefault: {
+        sseAlgorithm: "aws:kms",
+        kmsMasterKeyId: key.id,
+      }
+    }
+  }
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
