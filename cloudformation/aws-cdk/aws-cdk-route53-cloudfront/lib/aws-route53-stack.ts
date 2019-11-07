971c904c3ecf74d1efda32b5452e4d8d996f3ca2
jforge <github@jforge.de>
2019-11-07 07:45:04 +0100

    structural housekeeping for proper cloudfront setup

--- cloudformation/aws-cdk/aws-cdk-route53-cloudfront/lib/aws-route53-stack.ts
+++ cloudformation/aws-cdk/aws-cdk-route53-cloudfront/lib/aws-route53-stack.ts
@@ -1,13 +1,11 @@
 import cdk = require('@aws-cdk/core');
-import {
-  PublicHostedZone,
-  HostedZone,
-  ARecord,
-  ARecordProps,
-  AddressRecordTarget
-} from '@aws-cdk/aws-route53';
+import { AddressRecordTarget, ARecord, ARecordProps, HostedZone, IHostedZone, PublicHostedZone } from '@aws-cdk/aws-route53';
+import { CloudFrontTarget } from '@aws-cdk/aws-route53-targets';
 import { BucketProps } from '../lib/aws-s3-stack';
-import { BucketWebsiteTarget } from '@aws-cdk/aws-route53-targets';
+import { CloudFrontWebDistribution, SSLMethod, SecurityPolicyProtocol } from '@aws-cdk/aws-cloudfront';
+import { DnsValidatedCertificate } from '@aws-cdk/aws-certificatemanager';
+import { IBucket } from '@aws-cdk/aws-s3';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
 
 interface CombinedStackProps extends BucketProps {
   primaryHostedZoneName?: string
@@ -46,15 +44,80 @@ export class Route53StackPredefinedZone extends cdk.Stack {
       domainName: `${props.primaryHostedZoneName}.`
     });
 
-    // create an additional A record pointing to a bucket
+    // derive the site domain name
+    const siteDomain = this.buildSiteDomainName(props);
+    new cdk.CfnOutput(this, 'Site', { value: 'https://' + siteDomain });
+
+    // TLS certificate
+    const certificateArn = this.buildCertificate(siteDomain, zone);
+    new cdk.CfnOutput(this, 'Certificate', { value: certificateArn });
+
+    // CloudFront distribution that provides HTTPS
+    const distribution: CloudFrontWebDistribution = this.buildCloudfrontDistribution(
+      siteDomain,
+      certificateArn,
+      props.userBucket);
+    new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
+
+    // Deploy bucket content in Cloudfront distribution context
+    this.deployContentToBucket(props.userBucket, distribution);
+
+    // Route53 alias record for the CloudFront distribution
+    const aRecord: ARecord = this.buildAliasRecord(
+      zone,
+      `test-alias.${props.primaryHostedZoneName}.`,
+      distribution);
+    new cdk.CfnOutput(this, 'AliasRecord', { value: aRecord.domainName });
+
+  }
+
+  buildAliasRecord(zone: IHostedZone, recordName: string, distribution: CloudFrontWebDistribution): ARecord {
     const recordProps: ARecordProps = {
       zone: zone,
-      recordName: `test-alias.${props.primaryHostedZoneName}.`,
-      target: AddressRecordTarget.fromAlias(new BucketWebsiteTarget(props.userBucket)),
+      recordName: recordName,
+      target: AddressRecordTarget.fromAlias(new CloudFrontTarget(distribution)),
       ttl: cdk.Duration.minutes(5)
-    }
+    };
+    return new ARecord(this, "SiteAliasRecord", recordProps);
+  }
 
-    new ARecord(this, "AliasRecord-lab", recordProps);
+  buildSiteDomainName(props: CombinedStackProps): string {
+    return `test-alias.${props.primaryHostedZoneName}`;
+  }
 
+  buildCertificate(siteDomain: string, zone: IHostedZone): string {
+    return new DnsValidatedCertificate(this, 'SiteCertificate', {
+      domainName: siteDomain,
+      hostedZone: zone,
+      region: 'us-east-1'
+    }).certificateArn;
+  }
+
+  buildCloudfrontDistribution(siteDomain: string, certificateArn: string, bucket: IBucket): CloudFrontWebDistribution {
+    return new CloudFrontWebDistribution(this, 'SiteDistribution', {
+      aliasConfiguration: {
+        acmCertRef: certificateArn,
+        names: [siteDomain, `www.${siteDomain}`],
+        sslMethod: SSLMethod.SNI,
+        securityPolicy: SecurityPolicyProtocol.TLS_V1_2_2018,
+      },
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        }
+      ]
+    });
+  }
+
+  deployContentToBucket(bucket: IBucket, distribution: CloudFrontWebDistribution) {
+    new BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [Source.asset('./assets')],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
   }
 }
--- cloudformation/aws-cdk/aws-cdk-route53-cloudfront/lib/aws-s3-stack.ts
+++ cloudformation/aws-cdk/aws-cdk-route53-cloudfront/lib/aws-s3-stack.ts
@@ -1,7 +1,6 @@
 import cdk = require('@aws-cdk/core');
 import iam = require('@aws-cdk/aws-iam');
 import s3 = require('@aws-cdk/aws-s3');
-import s3deploy = require('@aws-cdk/aws-s3-deployment');
 
 export interface BucketProps extends cdk.StackProps {
   userBucket?: s3.IBucket;
@@ -15,24 +14,15 @@ export class BucketStack extends cdk.Stack {
 
     this.bucket = new s3.Bucket(this, 'CDKTestBucket', {
       bucketName: "test-lab-00x-de",
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
       publicReadAccess: true,
-      websiteIndexDocument: "index.html",
-      websiteErrorDocument: 'error.html'
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+
     });
     new cdk.CfnOutput(this, 'Bucket', { value: this.bucket.bucketName });
 
-    this.deployContent();
     this.createReadWriteUser();
-  }
-
-  deployContent() {
-    new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
-      sources: [ s3deploy.Source.asset('./assets') ],
-      destinationBucket: this.bucket,
-//      distribution,
-//      distributionPaths: ['/*'],
-    });
   }
 
   createReadWriteUser() {
