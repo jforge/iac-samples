1a5ebff02c8e008a02e1ded3c79cab59e091cdf9
jforge <github@jforge.de>
2020-02-10 07:37:46 +0100

    added sample lambda_handler

--- cloudformation/aws-cdk/simplified-init-template/index.ts
+++ cloudformation/aws-cdk/simplified-init-template/index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 
 export class SimplifiedInitTemplateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
